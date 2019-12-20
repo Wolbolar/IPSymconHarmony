b6b94a10a9c4f67c08c059e30cce0bf0983f2374
Fonzo <wolbolar@gmail.com>
2019-12-20 20:11:45 +0000

    Apply fixes from StyleCI

--- Harmony Hub/module.php
+++ Harmony Hub/module.php
@@ -1711,9 +1711,10 @@ Switch ($_IPS[\'SENDER\'])
      ***********************************************************/
 
     /**
-     * Polyfill for IP-Symcon 4.4 and older
+     * Polyfill for IP-Symcon 4.4 and older.
+     *
      * @param string $Ident
-     * @param mixed $Value
+     * @param mixed  $Value
      */
     //Add this Polyfill for IP-Symcon 4.4 and older
     protected function SetValue($Ident, $Value)
@@ -1725,11 +1726,11 @@ Switch ($_IPS[\'SENDER\'])
             SetValue($this->GetIDForIdent($Ident), $Value);
         }
         if ($Value === true) {
-            $valuestring = "true";
+            $valuestring = 'true';
         }
         if ($Value === false) {
-            $valuestring = "false";
+            $valuestring = 'false';
         }
-        $this->SendDebug("SetValue", "Set variable with objid" . $this->GetIDForIdent($Ident) . " and ident " . $Ident . " to " . $valuestring, 0);
+        $this->SendDebug('SetValue', 'Set variable with objid' . $this->GetIDForIdent($Ident) . ' and ident ' . $Ident . ' to ' . $valuestring, 0);
     }
 }
